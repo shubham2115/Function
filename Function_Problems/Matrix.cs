69c909564858997b25a8a439266260207c1a9d4b
shubham <s.shrikhande1@gmail.com>
2022-01-27 00:20:49 +0530

    [shubham added this file]

---
+++ Function_Problems/Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_Problems
+{
+    public class Matrix
+    {
+        
+        
+            public void Array()
+            {
+                int row, coloumn;
+                Console.WriteLine("Enter number of rows");
+                row = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number of coloumn");
+                coloumn = Convert.ToInt32(Console.ReadLine());
+                int[,] elements = new int[row, coloumn];
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < coloumn; j++)
+                    {
+                        Console.WriteLine("Enter element" + i + j);
+
+                        elements[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                for (int i = 0; i < row; i++)
+                {
+
+                    for (int j = 0; j < coloumn; j++)
+                    {
+                        Console.Write(elements[i, j]);
+                        Console.Write("\t");
+
+                    }
+                    Console.WriteLine("\n");
+                }
+
+            }
+
+        
+    }
+
+}
---
+++ Function_Problems/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Function_Problems.Matrix obj = new Function_Problems.Matrix();
+obj.Array();
